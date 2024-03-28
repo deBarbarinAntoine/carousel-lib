4f93685b86318fe62ba11fbd564982cc60903c6b
deBarbarinAntoine <antoine.debarbarin@ynov.com>
2024-03-28 22:15:13 +0100

    Toying with swiper parameters and functionalities.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,28 @@
 var swiper = new Swiper('.mySwiper', {
     spaceBetween: 30,
     loop: true,
+    autoplay: {
+      enabled: true,
+      pauseOnMouseEnter: true,
+    },
+    effect: 'coverflow',
+    coverflowEffect: {
+        scale: 0.9,
+        rotate: 10,
+        modifier: 0.7,
+    },
+    mousewheel: {
+        enabled: true,
+        releaseOnEdges: true,
+    },
+    keyboard: {
+      enabled: true,
+    },
     breakpoints: {
         // 0px <= window width < 768px (phones)
         0: {
             slidesPerView: 2,
-            spaceBetween: 20
+            spaceBetween: 20,
         },
         // 768px <= window width < 1024px (tablets)
         768: {
@@ -17,12 +34,13 @@ var swiper = new Swiper('.mySwiper', {
         // 1024px <= window width (desktop)
         1024: {
             slidesPerView: 5,
-            spaceBetween: 40
+            spaceBetween: 40,
         },
     },
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
+        dynamicBullets: true,
     },
     navigation: {
         prevEl: '.swiper-button-prev',
