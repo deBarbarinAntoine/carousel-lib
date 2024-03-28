667faffa40e6f9121f8be72532b468bd7249046e
deBarbarinAntoine <antoine.debarbarin@ynov.com>
2024-03-28 16:12:28 +0100

    Finished carousel for TP.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,31 @@
 "use strict"
 
-var swiper = new Swiper(".mySwiper", {
-    autoHeight: true,
-    slidesPerView: 3,
+var swiper = new Swiper('.mySwiper', {
     spaceBetween: 30,
+    loop: true,
+    breakpoints: {
+        // 0px <= window width < 768px (phones)
+        0: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // 768px <= window width < 1024px (tablets)
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        // 1024px <= window width (desktop)
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 40
+        },
+    },
     pagination: {
-        el: ".swiper-pagination",
+        el: '.swiper-pagination',
         clickable: true,
+    },
+    navigation: {
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
     },
 });
